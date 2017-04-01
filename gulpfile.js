3f34040ccd844f1b53bb7b4df1a185a1c6c0303e
Alberto Piras <a.piras.ict@gmail.com>
2017-04-01 15:40:01 +0200

    updating gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require('gulp');
+var del = require('del');
 var ts = require('gulp-typescript');
 var tsProject = ts.createProject('tsconfig.json');
 
-
-gulp.task('transpile', function () {
+//transpiles .TS files after cleaning oldjs files
+gulp.task('transpile', ['clean'], function () {
     var destDir =  '.';
 
     return gulp.src('*.ts')
         .pipe(ts(tsProject))
         .pipe(gulp.dest(destDir));
+});
+
+// Clean js files
+gulp.task('clean', function() {
+  return del(['demo-checkbox-ajax.js', 'checkbox-ajax.js']);
 });
